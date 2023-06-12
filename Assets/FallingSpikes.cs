31fb3c68524f0467063c8273380e0e6941dede6c
ze-antunes <jprantunes2000@gmail.com>
2023-06-12 21:20:29 +0100

    Update Repo

--- Assets/FallingSpikes.cs
+++ Assets/FallingSpikes.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class FallingSpikes : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Rigidbody2D rb;
+    BoxCollider2D boxCollider2D;
+
+    public float distance;
+    bool isFalling = false;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Physics2D.queriesStartInColliders = false;
+        if (isFalling == false)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, distance);
+
+            Debug.DrawRay(transform.position, Vector2.down * distance, Color.red);
+
+            if (hit.transform != null)
+            {
+                if (hit.transform.tag == "Player")
+                {
+                    rb.gravityScale = 5;
+                    isFalling = true;
+                }
+            }
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag ==  "Player")
+        {
+            FindObjectOfType<PlayerMovement>().TakeDamage(25);
+            Destroy(gameObject);
+        } if (other.gameObject.tag ==  "TilesetLayer"){
+            rb.gravityScale = 0;
+            boxCollider2D.enabled = false;
+        }
     }
 }
